759057f8450d42703643f98db53a0ce4a17223ca
123yajie321 <77299516+123yajie321@users.noreply.github.com>
2025-05-22 10:52:05 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "project1" {
     source = "./modules/network"
-    name_prefix = "testYajie"
+    name_prefix = var.name
     vnet_cidrs = ["10.0.0.0/16"]
     subnet_cidrs = ["10.0.0.0/24"]
     open_ports = ["80", "443"] 
